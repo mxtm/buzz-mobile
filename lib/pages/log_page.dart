8b28f5d476f737f7707725ef706e63ea9a8165e9
zbb00 <zbb00@mail.aub.edu>
2020-03-05 18:38:50 +0200

    Tabs implemented for navigation Log page added

---
+++ lib/pages/log_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LogPage extends StatefulWidget {
+  @override
+  _LogPageState createState() => _LogPageState();
+}
+
+class _LogPageState extends State<LogPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
